b195977ed84798e64523e94935dde7d360e940af
Eriq Augustine <eriq.augustine@gmail.com>
2016-11-07 10:23:39 -0800

    Some cleanup. Regenerated the fake data. Fixed an inssue where some version of python will complain about the difference between an ndarray and list.

--- code/features.py
+++ code/features.py
@@ -4,25 +4,20 @@ import numpy as np
 import sys
 
 # This is mainly for deciding if we are getting real or fake data.
-DEBUG = False
+DEBUG = True
 
 # TODO(dhawal): Do some feature engineering.
+# As of now, we are only returning numeric features.
 def getFeatures():
     features = []
     businesses = data.getBusinesses(DEBUG)
-
-    # print businesses[0]
-    # sys.exit()
 
     # one-hot encoding of active, city, and state variables + numeric features
     vec = DictVectorizer()
     cityStateMatrix = vec.fit_transform([{"active": b[1], "city":b[2], "state":b[3]} for b in businesses]).toarray()
     businesses = np.array([business[4:] for business in businesses])
-    features = np.hstack((cityStateMatrix, businesses))
+    features = np.hstack((cityStateMatrix, businesses)).tolist()
     return features
-   
-    # For now, just pick out the numeric features.
-    # return [business[3:] for business in businesses]
 
 if __name__ == '__main__':
-    getFeatures()
+    print(getFeatures())
