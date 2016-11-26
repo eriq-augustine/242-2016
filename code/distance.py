28e7f86d8cb32620cd8147e4b87b69c700cd0ab0
xiao li <sean@eduroam-169-233-210-224.ucsc.edu>
2016-11-26 00:30:59 -0800

    add mnhattan and needleman_wunsch

--- code/distance.py
+++ code/distance.py
@@ -1,11 +1,14 @@
 import math
-
+import numpy
 # A place for distance metric related things.
 # Do not feel limited to use only this file, but whoever wants to use distance things
 # should just have to import this one file.
 
 # TODO(xiao): Research and implement other distance metrics.
 
+'''
+The euclidean distance for numeric features.
+'''
 # Pre: |a| and |b| are collections of numeric values.
 def euclidean(a, b):
     assert(len(a) == len(b))
@@ -16,6 +19,21 @@ def euclidean(a, b):
 
     return math.sqrt(distance)
 
+'''
+The manhattan distance for numeric features. 
+'''
+# Pre: a and b are two string variables
+def manhattan(a, b):
+    assert(len(a) == len(b))
+
+    distance = 0
+    for i in range(len(a)):
+        distance += math.fabs(a[i] - b[i])
+    return distance
+
+'''
+The levenshtein distance for string features.
+'''
 # Pre: a and b are two string variables
 def levenshtein(a, b):
 
@@ -46,3 +64,32 @@ def levenshtein(a, b):
         for j in range(len(v0)):
             v0[j] = v1[j]
     return v1[len(b)]
+    
+'''
+The Needleman Wunsch distance for string features.
+Matches are given +1, mismatches are given -1 and indels are given -1
+'''
+# Pre: a and b are string variales
+def needleman_wunsch(a,b):
+    state = numpy.zeros((len(a) + 1, len(b) + 1))
+    # set the initial values for state matrix
+    for i in xrange(1, len(a) + 1):
+        state[i][0] = state[i - 1][0] - 1
+    for i in xrange(1, len(b) + 1):
+        state[0][i] = state[0][i - 1] - 1
+    
+    for i in xrange(1, len(a) + 1):
+        for j in xrange(1, len(b) + 1):
+            diagonal = state[i - 1][j - 1]
+            if a[i - 1] == b[j - 1]:
+                diagonal += 1
+            else:
+                diagonal -= 1
+            state[i][j] = max(diagonal, state[i - 1][j] - 1, state[i][j - 1] - 1)
+    return state[len(a)][len(b)]
+
+
+
+    
+
+
