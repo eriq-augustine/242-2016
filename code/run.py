aa4f9861fb56d090a3f7737cda1693f9a1ab5160
Eriq Augustine <eriq.augustine@gmail.com>
2016-11-29 00:36:03 -0800

    Commenting out some temporarily broken code. Someone missed commiting a file?

--- code/run.py
+++ code/run.py
@@ -18,11 +18,10 @@ def run():
         print("         %s" % (", ".join([str(x) for x in sorted([businesses[index].otherInfo['yelpId'] for index in clusters[i]])])))
 
     #Metrics
-    goldLabel = metrics.readGoldLabel("../data/groundtruth")
-    b_cluster = metrics.getClusterBusinessID(businesses, clusters)
-    randIndex = metrics.randIndex(b_cluster, goldLabel)
-    print("rand index:"  + str(randIndex))
-
+    # goldLabel = metrics.readGoldLabel("../data/groundtruth")
+    # b_cluster = metrics.getClusterBusinessID(businesses, clusters)
+    # randIndex = metrics.randIndex(b_cluster, goldLabel)
+    # print("rand index:"  + str(randIndex))
     
 if __name__ == '__main__':
     run()
